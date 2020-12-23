e293f6157c7c96fe097b08df27647d7f81d79ef3
Gabriel Oliveira <goliveiracod@gmail.com>
2020-12-23 10:00:42 -0300

    feat: authentication middleware for the traveler user

---
+++ src/modules/traveler/infra/http/express/middleware/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { auth } from '@config/auth';
+
+import { AppError } from '@shared/errors/AppError';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const decoded = verify(token, auth.jwt.publicKey);
+
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
